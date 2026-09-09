eecf1a7b07ee3d92b94e456975136fdb16495803
RoniR <ronirachid459@gmail.com>
2026-09-09 23:39:37 +0200

    Working condition with 3 examples. Improved the bresenham's methode to draw in all direction. Also fixed the drawGridIso and added x and y offset and scaling

--- Task4-3dMapDraw/fdfParser.c
+++ Task4-3dMapDraw/fdfParser.c
@@ -18,7 +18,6 @@ int* parseCords(char* s, int* size) {
         }
         counter++;
     }
-    printf("%d\n", len);
     *size = len;
     int* result = malloc(sizeof(int) * len);
 
@@ -27,10 +26,15 @@ int* parseCords(char* s, int* size) {
     while(s[counter]) {
         if (s[counter] != ' ' && s[counter] != '\n') {
             int x = 0;
+            int isNegative = -1;
             while(s[counter] && s[counter] != ' ' && s[counter] != '\n') {
-                x = x * 10 + s[counter] - '0';
+                if (s[counter] == '-') {
+                    isNegative = 1;
+                }
+                else x = x * 10 + s[counter] - '0';
                 counter++;
             }
+            if (isNegative == 1) x *= -1;
             result[len] = x;
             len++;
         }
@@ -39,13 +43,44 @@ int* parseCords(char* s, int* size) {
     return result;
 }
 
-void my_mlx_pixel_put(t_data *data, int x, int y, int color) {
+void my_mlx_pixel_put(t_data *data, int x, int y, unsigned int color) {
     printf("%d-%d\n", x, y);
 	char* dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
+// Uses Bresenham's to draw a line between 2 points in a grid, vertical
+void drawBetween2PointsVertic(int x1, int y1, int x2, int y2, unsigned int color, t_data data) {
+    if (y1 > y2) {
+        int temp = x1;
+        x1 = x2;
+        x2 = temp;
+        temp = y1;
+        y1 = y2;
+        y2 = temp;
+    }
+    int dx = x2 - x1;
+    int dy = y2 - y1;
 
-void drawBetween2Points(int x1, int y1, int x2, int y2, int color, t_data data) {
+    int dir = dx < 0 ? -1 : 1;
+    dx *= dir;
+
+    if (dy == 0) {
+        my_mlx_pixel_put(&data, x1, y1, color);
+        return;
+    }
+    int x = x1;
+    int D = 2 * dx - dy;
+    for (int i = 0; i <= dy; i++) {
+        my_mlx_pixel_put(&data, x, y1 + i, color);
+        if (D >= 0) {
+            x += dir;
+            D -= 2 * dy;
+        }
+        D += 2 * dx;
+    }
+}
+// Uses Bresenham's to draw a line between 2 points in a grid, horzintal
+void drawBetween2PointsHorz(int x1, int y1, int x2, int y2, unsigned int color, t_data data) {
     if (x1 > x2) {
         int temp = x1;
         x1 = x2;
@@ -54,10 +89,8 @@ void drawBetween2Points(int x1, int y1, int x2, int y2, int color, t_data data) 
         y1 = y2;
         y2 = temp;
     }
-
-    
-    int dx = x1 - x2;
-    int dy = y1 - y2;
+    int dx = x2 - x1;
+    int dy = y2 - y1;
 
     int dir = dy < 0 ? -1 : 1;
     dy *= dir;
@@ -78,23 +111,42 @@ void drawBetween2Points(int x1, int y1, int x2, int y2, int color, t_data data) 
     }
 }
 
+// Uses Bresenham's to draw a line between 2 points in a grid, chooses H or V
+void drawBetween2Points(int x1, int y1, int x2, int y2, unsigned int color, t_data data) {
+    if (abs(x2 - x1) > abs(y2 - y1)) {
+        drawBetween2PointsHorz(x1, y1, x2, y2, color, data);
+    }
+    else drawBetween2PointsVertic(x1, y1, x2, y2, color, data);
+}
 
 
-void drawGridIso(int rowLen, int colLen, int** grid, int color, t_data data) {
-    // x = (x-y) * cos(30 deg)
-    // y = (x+y) * sin(30 deg) - z
-    // Need to draw line from iso current to iso current right neigh and under neigh
-    double sin30 = sin(30);
-    double cos30 = cos(30);
+void drawGridIso(int rowLen, int colLen, int** grid, unsigned int color, t_data data) {
+    double rad30 = 0.52359877559;
+    double sin30 = sin(rad30);
+    double cos30 = cos(rad30);
+
+    int scale = 30;
+    int z_scale = 2;
+    int offset_x = 960;
+    int offset_y = 300;
+
     for (int y = 0; y < colLen; y++) {
         for (int x = 0; x < rowLen; x++) {
-            int currentX = (x-y) * cos30;
-            int currentY = (x+y) * sin30 - grid[y][x];
+            int currentX = (x - y) * scale * cos30 + offset_x;
+            int currentY = (x + y) * scale * sin30 - (grid[y][x] * z_scale) + offset_y;
+
+            // right neigh
             if (x < rowLen - 1) {
-                drawBetween2Points(currentX, currentY, (x+1-y) * cos30, (x+1+y) * sin30 - grid[y][x+1], color, data);
+                int rightX = ((x + 1) - y) * scale * cos30 + offset_x;
+                int rightY = ((x + 1) + y) * scale * sin30 - (grid[y][x + 1] * z_scale) + offset_y;
+                drawBetween2Points(currentX, currentY, rightX, rightY, color, data);
             }
+
+            // left neigh
             if (y < colLen - 1) {
-                drawBetween2Points(currentX, currentY, (x-y+1) * cos30, (x+y+1) * sin30 - grid[y+1][x], color, data);
+                int bottomX = (x - (y + 1)) * scale * cos30 + offset_x;
+                int bottomY = (x + (y + 1)) * scale * sin30 - (grid[y + 1][x] * z_scale) + offset_y;
+                drawBetween2Points(currentX, currentY, bottomX, bottomY, color, data);
             }
         }
     }
@@ -107,7 +159,7 @@ int drawMap(char* file) {
     int rowLen = -1;
     int colLen = 0;
     char* line;
-    int color = 0x00FF0000;
+    unsigned int color = 0x00FF0000;
 
     vars.mlx = mlx_init();
     vars.mlx_win = mlx_new_window(vars.mlx, 1920, 1080, "GRID");
@@ -120,12 +172,20 @@ int drawMap(char* file) {
         return -1;
     }
     // read the grid line by line and storing it
-    int** grid = malloc(sizeof(int*) * 10);
+    line = get_next_line(fd);
+    int counter = 0;
+    int len = 0;
+    while(line[counter]) {
+        if (line[counter] != ' ' && line[counter] != '\n' && (counter == 0 || line[counter-1] == ' ')) {
+            len++;
+        }
+        counter++;
+    }
+    int** grid = malloc(sizeof(int*) * len);
     if (grid == NULL) {
         printf("Error allocations on grid\n");
         return -1;
     }
-    line = get_next_line(fd);
     while(line != NULL) {
         printf("inLine\n");
         grid[colLen] = parseCords(line, &rowLen);
--- Task4-3dMapDraw/main.c
+++ Task4-3dMapDraw/main.c
@@ -1,6 +1,8 @@
 #include "3dMapDraw.h"
 
 int	main(void) {
-    drawMap("./simpleGrid.fdf");
+    //drawMap("./simpleGrid.fdf");
+    drawMap("./donut.fdf");
+    //drawMap("./platue.fdf");
     return 0;
 }
