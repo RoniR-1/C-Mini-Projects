aee888cd2aac3a05db15bad94e0ae479b65c90b6
RoniR <ronirachid459@gmail.com>
2026-09-17 22:25:27 +0200

    Implemented allocations of threads and mutex keys. Prints state of thread.

--- Task5-MulitThreading/mutli_threading.h
+++ Task5-MulitThreading/mutli_threading.h
@@ -1,13 +1,24 @@
-#include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <unistd.h>
 
-typedef struct s_phil {
+typedef struct s_phil_info {
     int     number_of_philosophers;
     int     time_to_die;
     int     time_to_eat;
     int     time_to_sleep;
     int     number_of_times_each_philosopher_must_eat;
+} t_phil_info;
+
+typedef struct s_phil {
+    int*            forks;
+    t_phil_info*    phil_info;
+    pthread_mutex_t*mutex;
+    pthread_t       id;
 } t_phil;
 
-void eat_spaghitti(t_phil*);
-int parse_phil(t_phil* phil, int argc, char** argv);
+
+void eat_spaghitti(t_phil_info*);
+int parse_phil(t_phil_info* phil, int argc, char** argv);
--- Task5-MulitThreading/philisophers.c
+++ Task5-MulitThreading/philisophers.c
@@ -18,7 +18,7 @@ int parse_string_to_int(int argc, char** argv) {
     return result;
 } 
 
-int parse_phil(t_phil* phil, int argc, char** argv) {
+int parse_phil(t_phil_info* phil, int argc, char** argv) {
     // prob can get away with a for loop but meh
     // TO DO: Should check weather they are all positive and return error
     phil->number_of_philosophers = parse_string_to_int(1, argv);
@@ -30,8 +30,70 @@ int parse_phil(t_phil* phil, int argc, char** argv) {
     return 0;
 }
 
+int eat() {
 
-void eat_spaghitti(t_phil*) {
+}
+int think() {
+
+}
+int sleep() {
+}
+
+void phil_life(void* arg) {
+    t_phil* phil = (t_phil*) arg;
+    struct timeval time1;
+    struct timeval time2;
+    if (gettimeofday(&time1, NULL) != 0) {free(phil); return;}
+    while (1) {
+        if (gettimeofday(&time2, NULL) != 0) break;
+        //eating
+        if (eat() != 0) break; // eating
+        //sleeping
+        if (gettimeofday(&time2, NULL) != 0) break;
+        printf("%ld.%06ld Professor %d is sleeping\n", time2.tv_sec, time2.tv_usec, phil->id);
+        usleep(phil->phil_info->time_to_sleep);
+        //thinking
+        if (gettimeofday(&time2, NULL) != 0) break;
+        printf("%ld.%06ld Professor %d is sleeping\n", time2.tv_sec, time2.tv_usec, phil->id);
+    }
+
+
+
+    free(phil);
+}
+
+
+void eat_spaghitti(t_phil_info* phils) {
     // phils are numbered from 1 to N
     // phil n sits next to n-1 and n+1 and phil 1 sits next to 2 and N
+    // #forks = #phil, to eat, phil needs 2 forks
+    int N = phils->number_of_philosophers;
+    pthread_mutex_t mutex_key = PTHREAD_MUTEX_INITIALIZER; // lock
+    //pthread_t* threads = calloc(N, sizeof(pthread_t));
+    int* forks = calloc(N, sizeof(int)); // if return --, take if == 0 and ++, else wait
+    
+    for (int i = 0; i < N; i++) {
+        t_phil *phil = malloc(sizeof(t_phil));
+        if (phil == NULL) {
+            printf("Malloc failed, exiting\n");
+            pthread_mutex_destroy(&mutex_key);
+            break;
+        }
+
+        phil->phil_info = phils;
+        phil->mutex = &mutex_key;
+        phil->forks = forks;
+
+        if (pthread_create(&phil->id, NULL, phil_life, phil) != 0) {
+            printf("Failed thread creation, exiting\n");
+            pthread_mutex_destroy(&mutex_key);
+            free(phil);
+            break;
+        }
+    }
+
+
+
+    pthread_mutex_destroy(&mutex_key);
+    free(forks); //free(phil);
 }
